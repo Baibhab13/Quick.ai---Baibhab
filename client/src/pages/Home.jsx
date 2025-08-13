daf5b087f7fb1f5cede47da480bed9655e17b0b8
Rabindrasenapaty <rabindrasenapaty2003@gmail.com>
2025-08-13 19:11:06 +0530

    testimonial complted

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,16 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Plan from '../components/Plan'
 import Footer from '../components/Footer'
+import Testimonial from '../components/Testimonial'
+import AiTools from '../components/AiTools'
 
 const Home = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <AiTools/>
+    <Testimonial/>
     <Plan/>
     <Footer/>
     </>
