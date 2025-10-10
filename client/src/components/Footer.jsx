559d1b8a6b33639c40b0252d44ddc01e7fc3b076
Rabindrasenapaty <rabindrasenapaty2003@gmail.com>
2025-10-10 23:32:26 +0530

    name changed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="pt-4 text-center text-xs md:text-sm pb-5">
-                Copyright 2025 ©Rabindra. All Right Reserved.
+                Copyright 2025 ©Chhayakant. All Right Reserved.
             </p>
         </footer>
   )
